b1e9bbf9a8f89958d5ed76d92407d47dfc87b7ef
damian.kaplon <kaplon.damian99@gmail.com>
2022-05-31 17:04:50 +0200

    Registration and getById

--- security-micro/src/main/java/pl/damiankaplon/service/JwtSecurityService.java
+++ security-micro/src/main/java/pl/damiankaplon/service/JwtSecurityService.java
@@ -2,12 +2,14 @@ package pl.damiankaplon.service;
 
 import io.smallrye.jwt.build.Jwt;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import pl.damiankaplon.entity.Account;
 import pl.damiankaplon.repository.AccountRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.security.auth.login.LoginException;
+import java.util.Optional;
 
 @ApplicationScoped
 @RequiredArgsConstructor
@@ -28,5 +30,27 @@ public class JwtSecurityService implements SecurityService{
                         .subject(account.getEmail())
                         .expiresIn(360).sign()
         );
+    }
+
+    @Override
+    public ObjectId register(Registration dto) throws LoginException {
+        Optional<Account> optAcc = accountRepository.findByEmail(dto.email());
+        if (optAcc.isPresent())
+            throw new LoginException("Email is already occupied");
+
+        Account account = Account.from(dto);
+        accountRepository.persist(account);
+
+        return account.getId();
+    }
+
+    @Override
+    public Optional<AccountDTO> getAccountById(String id) {
+        Optional<Account> account = accountRepository.findByIdOptional(new ObjectId(id));
+        return account.map(value -> new AccountDTO(
+                value.getEmail(),
+                value.getName(),
+                value.getSurname()
+        ));
     }
 }
--- security-micro/src/main/java/pl/damiankaplon/service/SecurityService.java
+++ security-micro/src/main/java/pl/damiankaplon/service/SecurityService.java
@@ -1,15 +1,20 @@
 package pl.damiankaplon.service;
 
 
+import org.bson.types.ObjectId;
+
 import javax.security.auth.login.LoginException;
-import java.util.UUID;
+import java.util.Optional;
 
 public interface SecurityService {
 
     record Credentials(String login, String password){}
     record BearerToken(String token){}
-    record AccountDTO(UUID uuid, String email, String name, String surname){}
+    record Registration(String email, String password, String name, String surname){}
+    record AccountDTO(String email, String name, String surname){}
 
     BearerToken getBearerToken(Credentials credentials) throws LoginException;
+    ObjectId register(Registration dto) throws LoginException;
+    Optional<AccountDTO> getAccountById(String id);
 
 }
