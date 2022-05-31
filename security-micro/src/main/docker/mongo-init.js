8bd4af3a089c19ff414401b57e071dbabf15c407
damian.kaplon <kaplon.damian99@gmail.com>
2022-05-31 14:50:53 +0200

    Add missing makefile

--- security-micro/src/main/docker/mongo-init.js
+++ security-micro/src/main/docker/mongo-init.js
@@ -1,2 +1,14 @@
 db = db.getSiblingDB('database-security');
 db.createCollection('accounts');
+db.createUser(
+    {
+        user: "admin",
+        pwd: "admin",
+        roles: [
+            {
+                role: "readWrite",
+                db: "database-security"
+            }
+        ]
+    }
+);
