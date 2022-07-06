1a28c858bc2d00cedc5c0c3bbb4f88fc476b1b4f
Damian Kaplon <damian.kaplon@globallogic.com>
2022-07-06 09:23:06 +0200

    Create local storage dir programatically

--- image-storage-micro/src/main/java/pl/damiankaplon/ImageService.java
+++ image-storage-micro/src/main/java/pl/damiankaplon/ImageService.java
@@ -3,6 +3,7 @@ package pl.damiankaplon;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.bson.types.ObjectId;
 
@@ -10,14 +11,17 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
 @ApplicationScoped
 @RequiredArgsConstructor
+@Slf4j
 public class ImageService {
 
     private final ImageRepositoryMongo imageRepositoryMongo;
@@ -30,7 +34,22 @@ public class ImageService {
             FILE_SYSTEM_ROOT = "";
         else
             FILE_SYSTEM_ROOT = "/";
+        createLocalStorageFolder();
     }
+
+    private static void createLocalStorageFolder() {
+        try {
+            Files.createDirectory(Paths.get(FILE_SYSTEM_ROOT, "local-storage"));
+        } catch (FileAlreadyExistsException ex) {
+            log.warn("Could not create local image storage dir. It might already exist");
+        }
+        catch (IOException ex) {
+            log.error("IO Exception thrown while creating local image storage dir.");
+            log.error(ex.getMessage());
+            Arrays.stream(ex.getStackTrace()).forEach(st -> log.error(st.toString()));
+        }
+    }
+
     public Multi<File> getMultiImages(ObjectId candyId) {
         Multi<ImageEntity> multiEntities = imageRepositoryMongo.findByCandyId(candyId);
         Function<? super ImageEntity, File> toFile = (entity) -> new File(Paths.get(FILE_SYSTEM_ROOT, entity.getPath()).toString());
